051d29782d02f659f4cf125b1aee81d075e40862
Boyan Peychev <boyan@peychev.net>
2026-02-02 17:46:21 +0200

    Update __init__.py

--- octodns_cloudns/__init__.py
+++ octodns_cloudns/__init__.py
@@ -8,7 +8,7 @@ from octodns.record import Record
 logging.basicConfig(level=logging.DEBUG)
 logger = logging.getLogger(__name__)
 
-__version__ = __VERSION__ = '0.0.10'
+__version__ = __VERSION__ = '0.0.12'
 
 class ClouDNSClientException(ProviderException):
     pass
